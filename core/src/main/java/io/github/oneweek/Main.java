13a8626dad0f7dabb7d6f8734ad6e7fbb52023a4
unknown <garciaantoniojr190@gmail.com>
2025-02-19 21:40:30 +0800

    added player and animation

--- core/src/main/java/io/github/oneweek/Main.java
+++ core/src/main/java/io/github/oneweek/Main.java
@@ -14,10 +14,11 @@ public class Main extends ApplicationAdapter {
     SpriteBatch batch;
     Camera camera;
 	Texture img;
+    Player player;
 
     int speed = 200;
     float accelaration = 50;
-    float deceleration = 1;
+    float deceleration = 50;
 
     ParallaxLayer[] layers;
     @Override
@@ -25,9 +26,10 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
 		// Viewport size the same as the background texture
 		camera = new OrthographicCamera(1920, 1080);
+        player = new Player(200, -320);
 
 		// Art assets from
-		// https://opengameart.org/content/parallax-2d-backgrounds
+        // https://opengameart.org/content/parallax-2d-backgrounds
 		layers = new ParallaxLayer[10];
 		layers[0] = new ParallaxLayer(new Texture("Background/0.png"), 0.1f, true, false);
 		layers[1] = new ParallaxLayer(new Texture("Background/1.png"), 0.2f, true, false);
@@ -47,6 +49,8 @@ public class Main extends ApplicationAdapter {
 		}
     }
 
+
+
     @Override
     public void render () {
 
@@ -55,18 +59,19 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.047f, 0.067f, 0.133f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float deltaTime = Gdx.graphics.getDeltaTime();
 
 		batch.begin();
 		batch.draw(layers[0].texture, camera.position.x - camera.viewportWidth / 2,  camera.viewportHeight / 2, camera.viewportWidth, camera.viewportHeight);
 
 
-        if(!Gdx.input.isKeyPressed(Input.Keys.SPACE) && speed < 300){
+        if(!Gdx.input.isKeyPressed(Input.Keys.SPACE) && speed < 300 && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
             speed += accelaration;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && speed > 0) {
             // Gradually reduce speed when SPACE is not pressed
 
-            speed -= deceleration * Gdx.graphics.getDeltaTime();
+            speed -= deceleration;
 
 
 
@@ -80,6 +85,23 @@ public class Main extends ApplicationAdapter {
 		for (ParallaxLayer layer : layers) {
 			layer.render(batch);
 		}
+
+        // Keep player fixed relative to camera
+        float playerOffsetX = -800; // Adjust this for horizontal placement
+        float playerOffsetY = -280; // Adjust this for vertical placement
+        player.setPosition(camera.position.x + playerOffsetX, camera.position.y + playerOffsetY);
+
+        // Update and render the player
+        player.update(deltaTime);
+        player.render(batch);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            player.setState(Player.PlayerState.IDLE);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            player.setState(Player.PlayerState.ATTACKING);
+        } else {
+            player.setState(Player.PlayerState.RUNNING);
+        }
 		batch.end();
 	}
 
@@ -87,7 +109,8 @@ public class Main extends ApplicationAdapter {
 
 	@Override
 	public void dispose () {
-		batch.dispose();
+        batch.dispose();
+
 	}
 }
 
---
+++ core/src/main/java/io/github/oneweek/Player.java
@@ -0,0 +1,83 @@
+package io.github.oneweek;
+
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.Gdx;
+
+public class Player {
+    private Animation<TextureRegion> runAnimation, idleAnimation, attackAnimation, dieAnimation;
+    private float stateTime;
+    private Vector2 position;
+    private PlayerState currentState;
+
+    public enum PlayerState {
+        RUNNING, IDLE, ATTACKING, DYING
+    }
+
+    public Player(float x, float y) {
+        this.position = new Vector2(x, y);
+        this.stateTime = 0;
+        this.currentState = PlayerState.RUNNING; // Default state
+
+        // Load animations from PNG files
+        runAnimation = loadAnimation("Character/Run/", 16, 0.08f, Animation.PlayMode.LOOP);
+        idleAnimation = loadAnimation("Character/Idle/", 10, 0.2f, Animation.PlayMode.LOOP);
+        attackAnimation = loadAnimation("Character/Attack/", 7, 0.08f, Animation.PlayMode.NORMAL);
+        dieAnimation = loadAnimation("Character/Hurt/", 4, 0.2f, Animation.PlayMode.NORMAL);
+    }
+
+    private Animation<TextureRegion> loadAnimation(String basePath, int frameCount, float frameDuration, Animation.PlayMode mode) {
+        Array<TextureRegion> frames = new Array<>();
+        for (int i = 1; i <= frameCount; i++) {
+            Texture texture = new Texture(Gdx.files.internal(basePath + i + ".png"));
+            frames.add(new TextureRegion(texture));
+        }
+        Animation<TextureRegion> animation = new Animation<>(frameDuration, frames);
+        animation.setPlayMode(mode);
+        return animation;
+    }
+
+    public void update(float deltaTime) {
+        stateTime += deltaTime;
+
+        // If dying animation finished, stay on the last frame
+        if (currentState == PlayerState.DYING && dieAnimation.isAnimationFinished(stateTime)) {
+            stateTime = dieAnimation.getAnimationDuration();
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        TextureRegion currentFrame = getCurrentFrame();
+        float scale = 5.0f; // Adjust scale factor (increase for bigger character)
+        batch.draw(currentFrame, position.x, position.y,
+            currentFrame.getRegionWidth() * scale,
+            currentFrame.getRegionHeight() * scale);
+    }
+
+    public void setPosition(float x, float y) {
+        this.position.set(x, y);
+    }
+
+    private TextureRegion getCurrentFrame() {
+        switch (currentState) {
+            case IDLE:
+                return idleAnimation.getKeyFrame(stateTime);
+            case ATTACKING:
+                return attackAnimation.getKeyFrame(stateTime, false);
+            case DYING:
+                return dieAnimation.getKeyFrame(stateTime, false);
+            case RUNNING:
+            default:
+                return runAnimation.getKeyFrame(stateTime);
+        }
+    }
+
+    public void setState(PlayerState newState) {
+        if (this.currentState != newState) {
+            this.currentState = newState;
+            stateTime = 0; // Reset animation time on state change
+        }
+    }
+}
