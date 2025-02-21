71269de6338ed8de5fce3b0b3d3f8d82e96ecb1c
Elaijah <elaijahangelo@gmail.com>
2025-02-21 17:07:46 +0800

    files update

--- core/src/main/java/io/github/oneweek/Main.java
+++ core/src/main/java/io/github/oneweek/Main.java
@@ -1,15 +1,44 @@
 package io.github.oneweek;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Main extends Game {
     public SpriteBatch batch;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        this.setScreen(new MenuScreen(this)); // Start with the Menu Screen
+
+        atlas = new TextureAtlas(Gdx.files.internal("atlas/game_atlas.atlas"));
+        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+        font = new BitmapFont(Gdx.files.internal("skin/default.fnt"));
+
+        this.setScreen(new MenuScreen(this));
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public TextureAtlas getTextureAtlas() {
+        return atlas;
+    }
+
+    public Skin getSkin() {
+        return skin;
+    }
+
+    public BitmapFont getFont() {
+        return font;
     }
 
     @Override
